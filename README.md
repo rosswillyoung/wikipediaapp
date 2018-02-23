6e46641e197a8eca045d99a483e9fd072c826f36
rosswillyoung <rosswillyoung@gmail.com>
2018-02-23 15:41:42 -0500

    Created the main logic of the page. Really feels thrown together but I am really enjoying learning about API calls and jQuery

---
+++ README.md
@@ -0,0 +1,4 @@
+A Wikipedia article searcher based on freecodecamps curriculum.
+
+It's been a great exercise to practice API calls and jQuery. Right now it feels completely thrown together
+but I'm sure that will get better with experience.
--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 let randurl = "https://www.wikipedia.org/wiki/Special:Random";
-let apiurl = "https://www.wikipedia.org/w/api.php?";
 
 $( "#randWiki" ).html(
         "<a href=" + randurl + ">Click to get a random wikipedia page!</a>"
         );
 
 
-function test(callback) {
-    apiurl += $.param({
-        'action': 'opensearch',
-        'search': 'SanFrancisco',
-        'format': 'json',
-        'limit': 10
-    });
-    //console.log(apiurl);
-    $.ajax({
-        url: apiurl,
-        dataType: 'jsonp',
-        success: function(a) {
-            callback(a);
-        }
-    })
-}
-test(function(a){
-    //console.log(a[3]);
-    for (let i = 0; i < a[3].length; i++) {
-        $("#container").append(
-            "<div class='entry'><a href='" + a[3][i] + "'</a>" + a[3][i] + "</div>"); 
+/*
+ * Get the search value and perform the api call
+ */
+function getWiki() {
+    var apiurl = "https://www.wikipedia.org/w/api.php?";
+    var term = $('#search').find('input[name="wiki"]').val().replace(/ /g, '_');
+
+    $('#container').html(''); 
+    //console.log(term);
+    function test(callback) {
+        apiurl += $.param({
+            'action': 'opensearch',
+            'search': term,
+            'format': 'json',
+            'limit': 10
+        });
+
+        //console.log(apiurl);
+        $.ajax({
+            url: apiurl,
+            dataType: 'jsonp',
+            success: function(a) {
+                callback(a);
+                console.log(a);
+            }
+            
+        })
     }
-});
+    test(function(a){
+        //console.log(a[3]);
+        for (let i = 0; i < a[1].length; i++) {
+            $("#container").append(
+                "<div id='entry" + i + "'><a href='" + a[3][i] + "'</a>" + a[1][i] + "<div id='summary" + i + "'></div></div>"); 
+            /*
+             * Get the summary of the wikipedia article and display it when hovered
+             */
+            $.getJSON('https://en.wikipedia.org/api/rest_v1/page/summary/' +
+                    a[1][i].replace(/ /g, '_'), function(data) {
+                        //console.log(data['extract']);
+                        $("#summary" + i).html(data['extract']);
+                        $("#summary" + i).css('display', 'none');
+                        $('#entry' + i).mouseenter(function() {
+                            $('#summary' + i).css('display', 'block');
+                        });
+                        $('#entry' + i).mouseleave(function() {
+                            $('#summary' + i).css('display', 'none');
+                        });
+                    });
+        }
+    });
+
+}
+
+
 
 
